02135553229385d43af75035953e42565a851875
Adrien Basso-Blandin <cypher_dechu@hotmail.com>
2015-06-12 21:03:20 +0200

    Revert "old version"
    This reverts commit 99e4cd357b209fec16804ed3b11ab4736b46bcc4.

--- exception.ml
+++
@@ -1,8 +0,0 @@
-let current_line = ref 1;;
-exception NotFinishException_e of string ;; (* point du compilateur non developpée *)
-exception NoInputFile_e of string ;; (* pas de fichier en entrée *)
-exception Emptyfile_e of string ;; (* le fichier d'entrée est vide *)
-exception Parsing_exception_e of string * int * string ;; (* erreur lors du parsing : variable non allouée *)
-exception LexException of string * string * Lexing.position * int ;;(* message ,chaine matchée, position (ligne + premier caractere), dernier offset  *)
-let parse_error msg arg = raise (Parsing_exception_e(msg,!current_line,arg));;
-exception BDDParsing_exception_e of string ;;
--- unification.ml
+++
@@ -1,9 +0,0 @@
-(* ***************** définition de sigma : substitution de variables *************** *)
-module Sigma_map = Map.Make ( 
-  struct 
-    type t = (atom_t * att_list_t)
-    let compare = Pervasives.compare
-  end)
-;; 
-(* sigma est une substitution d'un atome et de ses attributs par un autre *)
-type sigma_map_t = (atom_t * att_list_t) Sigma_map.t  ;;
